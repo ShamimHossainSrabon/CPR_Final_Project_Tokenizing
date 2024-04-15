bb0ab7212ed924bde0931e2f773f84522ad94501
ShamimHossainSrabon <shsrabon@myseneca.ca>
2024-04-14 22:43:46 -0400

    Version 2

---
+++ Version2/tokenizing.h
@@ -0,0 +1,55 @@
+/*
+General Information on
+===========================
+Source file Name: tokenizing.h
+
+TOKENIZING MODULE HEADER
+This is the header file of string tokenizing operation
+
+The main purpose of the file 
+1. Include Macro
+2. Include necessary C libraries.
+3. Include user-defined libraries
+
+Credit:
+Author          : Shamim Hossain Srabon
+Group Name		: Bb 4
+Group Members	: Students of Seneca College
+Course 			: CPR 101
+
+Supervisor		: Professor Raymond Wong
+
+Revision		: 1.0.5
+Version 		: 1
+
+License 		: Not Defined 
+
+Release date	: 16 April 2024
+Support			: mrkarim6@myseneca.ca
+*/
+
+// Start program
+
+/* 
+ * If the macro is defined or not 
+ * Define it if not defined already
+ */
+#ifndef _TOKENIZING_H_
+//
+// Define the macro _TOKENIZING_H_
+#define _TOKENIZING_H_
+//
+// Include the stander input and output C libraries.
+#include <stdio.h>
+//
+// Include the stander string C libraries.
+#include <string.h>
+//
+// Declare the prototype of fx tokenizing.
+// The function does not take or return any parameters.
+void tokenizing(void);
+
+// End the if conditoin
+#endif
+
+// End program
